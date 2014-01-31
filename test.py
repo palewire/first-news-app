a9d2cfa1713e1bd0479c81e355a581972cf7bd79
palewire <ben.welsh@gmail.com>
2014-01-31 09:57:17 -0800

    Added unittest and coverage report

---
+++ test.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+import unittest
+import app
+import freeze
+
+
+class AppTest(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.app.test_client()
+
+    def test_app(self):
+        self.app.get('/')
+        self.app.get('/1/')
+
+    def test_freeze(self):
+        freeze.freezer.freeze()
+
+
+if __name__ == '__main__':
+    unittest.main()
