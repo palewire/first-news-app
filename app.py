c4e437708effb00cd9055ee453a753a7f12a86ee
palewire <ben.welsh@gmail.com>
2014-01-31 09:37:53 -0800

    PEP8 and PyFlakes

--- app.py
+++ app.py
@@ -12,20 +12,16 @@ csv_dict = dict([[o['id'], o] for o in csv_list])
 
 @app.route("/")
 def index():
-    return render_template('index.html',
-        object_list=csv_list,
-    )
+    return render_template('index.html', object_list=csv_list)
 
 
 @app.route('/<number>/')
 def detail(number):
-    return render_template('detail.html',
-        object=csv_dict[number],
-    )
+    return render_template('detail.html', object=csv_dict[number])
 
 
 if __name__ == '__main__':
-    app.run( 
+    app.run(
         host="0.0.0.0",
         port=8000,
         use_reloader=True,
