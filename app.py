6944376ea2677874800f10f3361fbb98511801e6
palewire <ben.welsh@gmail.com>
2015-02-09 08:35:02 -0800

    A little more verbose for #29

--- app.py
+++ app.py
@@ -4,20 +4,27 @@ from flask import render_template
 app = Flask(__name__)
 
 
-csv_path = './static/la-riots-deaths.csv'
-csv_obj = csv.DictReader(open(csv_path, 'r'))
-csv_list = list(csv_obj)
-csv_dict = dict([[o['id'], o] for o in csv_list])
+def get_csv():
+    csv_path = './static/la-riots-deaths.csv'
+    csv_file = open(csv_path, 'r')
+    csv_obj = csv.DictReader(csv_file)
+    csv_list = list(csv_obj)
+    return csv_list
 
 
 @app.route("/")
 def index():
-    return render_template('index.html', object_list=csv_list)
+    template = 'index.html'
+    object_list = get_csv()
+    return render_template(template, object_list=object_list)
 
 
-@app.route('/<number>/')
-def detail(number):
-    return render_template('detail.html', object=csv_dict[number])
+@app.route('/<row_id>/')
+def detail(row_id):
+    template = 'detail.html'
+    object_list = get_csv()
+    row = [d for d in object_list if d['id'] == row_id][0]
+    return render_template(template, object=row)
 
 
 if __name__ == '__main__':
