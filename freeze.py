132e67e28680fe95d40edd9705c9042f6a2bee6e
palewire <ben.welsh@gmail.com>
2015-02-09 08:46:28 -0800

    Fixed freeze.py

--- freeze.py
+++ freeze.py
@@ -1,13 +1,13 @@
 from flask_frozen import Freezer
-from app import app, csv_list
+from app import app, get_csv
 freezer = Freezer(app)
 app.config['FREEZER_RELATIVE_URLS'] = True
 
 
 @freezer.register_generator
 def detail():
-    for row in csv_list:
-        yield {'number': row['id']}
+    for row in get_csv():
+        yield {'row_id': row['id']}
 
 
 if __name__ == '__main__':
