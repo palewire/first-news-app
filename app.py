436ab704bcb6a9e506846f2113e29b7211edb407
palewire <ben.welsh@gmail.com>
2016-03-05 15:35:24 -0800

    Fix

--- app.py
+++ app.py
@@ -5,29 +5,29 @@ from flask import render_template
 app = Flask(__name__)
 
 
-# def get_csv():
-#     csv_path = './static/la-riots-deaths.csv'
-#     csv_file = open(csv_path, 'r')
-#     csv_obj = csv.DictReader(csv_file)
-#     csv_list = list(csv_obj)
-#     return csv_list
-#
-#
-# @app.route("/")
-# def index():
-#     template = 'index.html'
-#     object_list = get_csv()
-#     return render_template(template, object_list=object_list)
-#
-#
-# @app.route('/<row_id>/')
-# def detail(row_id):
-#     template = 'detail.html'
-#     object_list = get_csv()
-#     for row in object_list:
-#         if row['id'] == row_id:
-#             return render_template(template, object=row)
-#     abort(404)
+def get_csv():
+    csv_path = './static/la-riots-deaths.csv'
+    csv_file = open(csv_path, 'r')
+    csv_obj = csv.DictReader(csv_file)
+    csv_list = list(csv_obj)
+    return csv_list
+
+
+@app.route("/")
+def index():
+    template = 'index.html'
+    object_list = get_csv()
+    return render_template(template, object_list=object_list)
+
+
+@app.route('/<row_id>/')
+def detail(row_id):
+    template = 'detail.html'
+    object_list = get_csv()
+    for row in object_list:
+        if row['id'] == row_id:
+            return render_template(template, object=row)
+    abort(404)
 
 
 if __name__ == '__main__':
